6e05ea9c2d0d2e0fd217ccc20acc5c5479dafb8a
MatheusSuzart <mathsuzart@outlook.com>
2022-04-15 23:50:52 -0300

    socket

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-
+const io = socketIO(server);
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public'))); // vai ler a pasta public
 
+io.on('connection', (socket)=>{
+    console.log("connection");
+});
